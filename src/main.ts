a58c45552f14d6dece5381c99a9b8ec1966da98f
0715yk <yongkiniiii@gmail.com>
2023-10-05 22:22:11 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import "./style.css";
+
+const thumbnailTemplate = document.getElementById("thumbnail-preview");
+
+const inputTitle = document.querySelector(".input-title");
+const inputSubTitle = document.querySelector(".input-subtitle");
+const inputContents = document.querySelector(".input-contents");
+
+const renderTitle = document.querySelector(".render.title");
+const renderSubTitle = document.querySelector(".render.subtitle");
+const renderContents = document.querySelector(".render.contents");
+
+inputTitle?.addEventListener("input", function (event) {
+  const target = event.target as HTMLInputElement;
+
+  if (target && renderTitle !== null) {
+    renderTitle.textContent = target.value;
+  }
+});
+
+inputSubTitle?.addEventListener("input", function (event) {
+  const target = event.target as HTMLInputElement;
+
+  if (target && renderSubTitle !== null) {
+    renderSubTitle.textContent = target.value;
+  }
+});
+
+inputContents?.addEventListener("input", function (event) {
+  const target = event.target as HTMLInputElement;
+
+  if (target && renderContents !== null) {
+    renderContents.textContent = target.value;
+  }
+});
